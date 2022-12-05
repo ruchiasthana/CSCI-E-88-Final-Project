7ce37b5abf0ffa1a10ebd73eb83cb05c15f3f6d8
Ruchi Asthana <rua490@g.harvard.edu>
2022-12-05 01:21:56 -0500

    Fixed Bug in Sentiment Analysis Code

--- apply_sentiment_analysis_with_aws_comprehend.py
+++ apply_sentiment_analysis_with_aws_comprehend.py
@@ -3,7 +3,6 @@ The goal of this document is to apply sentiment anlysis on tweet data
 We will be leveraging AWS Comprehend for sentiment analysis
 """
 
-import json
 import boto3
 import pandas as pd
 
@@ -13,23 +12,29 @@ client = boto3.client('comprehend')
 
 #We start by reading in a CSV file of elon_musks most recent tweets
 elon_musk_recent_tweets = pd.read_csv('./elon_musk_recent_tweets/elon_musk_recent_2000_tweet_data.csv')
-head = elon_musk_recent_tweets.head()
-print("The head of this data file is: ", head )
 
-#Then we pass each tweet to the AWS Comprehend API for sentiment analysis
+#Initialize fields that we will want to populate with our analysis
+rows, cols = elon_musk_recent_tweets.shape
+elon_musk_recent_tweets['sentiment'] = 'initial sentiment'
+elon_musk_recent_tweets['positive_sentiment_score'] = 0.0
+elon_musk_recent_tweets['negative_sentiment_score'] = 0.0
+
+#Pass each tweet to the AWS Comprehend API for sentiment analysis
 count = 0
-for tweet_text in elon_musk_recent_tweets['text']:
+for row_idx in range(rows):
+    tweet_text = elon_musk_recent_tweets['text'][row_idx]
     sentiment_analysis = client.detect_sentiment(Text=tweet_text, LanguageCode='en')
+    #Extract Relevant fields from AWS Comprehend Sentiment Analysis
     sentiment = sentiment_analysis['Sentiment']
     positive_sentiment_score = sentiment_analysis['SentimentScore']['Positive']
     negative_sentiment_score = sentiment_analysis['SentimentScore']['Negative']
-    elon_musk_recent_tweets['sentiment'] = sentiment
-    elon_musk_recent_tweets['positive_sentiment_score'] = positive_sentiment_score
-    elon_musk_recent_tweets['negative_sentiment_score'] = negative_sentiment_score
-    print("Tweet Text Number {}".format(count))
+    elon_musk_recent_tweets['sentiment'][row_idx] = sentiment
+    elon_musk_recent_tweets['positive_sentiment_score'][row_idx] = positive_sentiment_score
+    elon_musk_recent_tweets['negative_sentiment_score'][row_idx] = negative_sentiment_score
+    print("Tweet Text Number {} has been passed through Sentiment Analysis".format(count))
     count += 1
 
-print(elon_musk_recent_tweets.head())
+print(elon_musk_recent_tweets)
 elon_musk_recent_tweets.to_csv("elon_musk_recent_2000_tweet_data_with_sentiment_analysis.csv")
 
 

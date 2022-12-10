6bb14970b27fde00372a56aa5ed8a5b087664428
Ruchi Asthana <rua490@g.harvard.edu>
2022-12-10 18:20:56 -0500

    Updated s3 buckets in Kafka Consumer

--- Kafka_consumer.py
+++ Kafka_consumer.py
@@ -5,11 +5,12 @@ import json
 with open('config.json') as json_file:
     data = json.load(json_file)
 
-client= boto3.resource("s3",aws_access_key_id=data['AWS_ACCESS_KEY_ID'], aws_secret_access_key= data['AWS_SECRET_ACCESS_KEY'])
-
+client= boto3.resource("s3", aws_access_key_id=data['AWS_ACCESS_KEY_ID'], aws_secret_access_key= data['AWS_SECRET_ACCESS_KEY'])
+elon_musk_recent_tweets_with_sentiment_bucket = 'elon-musk-recent-tweets-with-sentiment-bucket-1'
+client.create_bucket(Bucket=elon_musk_recent_tweets_with_sentiment_bucket)
 consumer = KafkaConsumer('finalproject', bootstrap_servers=['localhost:9092'])
 
-s3_file_name = "s3://elon-musk-recent-tweets/TwitterData/Tweet.csv"
+s3_file_name = "elon_musk_recent_20000_tweet_data_with_sentiment_analysis.csv"
 
 
 for message in consumer:
@@ -17,3 +18,6 @@ for message in consumer:
     with open(s3_file_name, 'w') as f:
         print(message.value)
         f.write(f"{values}\n")
+
+boto3.client('s3').upload_file(elon_musk_recent_with_sentiment_analysis, elon_musk_recent_tweets_with_sentiment_bucket,
+                               'elon_musk_recent_20000_tweet_data_with_sentiment_analysis.csv')
